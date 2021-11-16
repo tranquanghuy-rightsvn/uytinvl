9b7a40b71127d40e46d60a10f8136a5718583b4a
tranquanghuy-rightsvn <tranquang.huy@rights-s.jp>
2021-11-16 23:18:20 +0700

    Fix dropdown css

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -27,6 +27,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def sign_up_params
-    params.require(:user).permit(:name, :email, :password)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
