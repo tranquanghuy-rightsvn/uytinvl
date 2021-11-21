da99ce48a7d19772db044112863ba8470e5ce515
Bac Xa lone na <bacxa0610@mail.com>
2021-11-21 14:40:48 +0700

    Create Post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  def show 
+    @post = Post.find_by(id: params[:id])
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :content, :category_id)
+    end
+end
+
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ApplicationRecord
   has_many :posts
   validates :name, presence: true
+
+  def self.generate_options
+    pluck(:name, :id)
+  end
 end
