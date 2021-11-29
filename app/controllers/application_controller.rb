fa4666c9bd7e42468021a223f2a4613538248701
tranquanghuy-rightsvn <tranquang.huy@rights-s.jp>
2021-11-29 18:31:46 +0700

    Edit index html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,12 @@ class ApplicationController < ActionController::Base
   def load_new_user
     @user = User.new
   end
+
+  def check_current_user
+    redirect_to root_path unless current_user
+  end
+
+  def redirect_to_root_path
+    redirect_to root_path
+  end
 end
--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -30,14 +30,14 @@ $(document).ready(function() {
   }); 
 
   $('#inputImagePost').change(function(){
-      const file = this.files[0];
-      if (file){
-        let reader = new FileReader();
-        reader.onload = function(event){
-          console.log(event.target.result);
-          $('#imagePost').attr('src', event.target.result);
-        }
-        reader.readAsDataURL(file);
+    const file = this.files[0];
+    if (file){
+      let reader = new FileReader();
+      reader.onload = function(event){
+        console.log(event.target.result);
+        $('#imagePost').attr('src', event.target.result);
       }
-    });
+      reader.readAsDataURL(file);
+    }
+  });
 });
