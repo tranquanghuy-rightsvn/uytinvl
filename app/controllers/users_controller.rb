e745c2d31f67962ee1b6430324421b32491e12a3
tranquanghuy-rightsvn <tranquang.huy@rights-s.jp>
2021-11-30 23:35:50 +0700

    Add ajax for user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,10 @@ class UsersController < ApplicationController
   def edit; end
 
   def update
-    if current_user.update! user_params
-      redirect_to root_path
-    else
-      render :edit
+    current_user.update user_params
+    
+    respond_to do |format|
+      format.js
     end
   end
 
